30e2deb342205ca974876c258cabf0497fe44643
Shalujha <Shalujha0907@users.noreply.github.com>
2025-06-18 16:58:32 +0530

    Added the input bar

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import type { task } from './type.ts';
 
@@ -37,30 +38,67 @@ const fakeTodo = [
 
 function serveTasks(tasks: task[]) {
   return tasks.map((task) => {
-    return <div key={task.taskId}>{task.taskName}</div>
+    return <div
+      key={task.taskId}>{task.taskName}
+      <button>remove</button>
+      <button>status</button>
+    </div>
 
   })
 }
 
 function serveTodo() {
   return fakeTodo.map((todo) => {
-    return <div key={todo.todoId}>
-      <h2>{todo.todoName}</h2>
+    return <div id='todo' key={todo.todoId}>
+      <div id='todo_title'>
+        <h4 id='todo_name'>{todo.todoName}</h4>
+        <button id='remove_button'>remove</button>
+      </div>
       {serveTasks(todo.tasks)}
 
     </div>
   })
+}
 
+function handleSubmit(title) {
+  console.log(title);
+
+}
+
+function createTitle() {
+  return <h1>Task Manager!!</h1>
+}
+
+function createInputTitle() {
+  const [title, setTitle] = useState("")
+
+  return <div id='input_bar'>
+    <input type="text"
+      name='todoTitle'
+      value={title}
+      placeholder='Enter the todo title'
+      onChange={(e) => setTitle(e.target.value)}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          return handleSubmit(e)
+        }
+      }}
+      autoFocus
+      required />
+    <button onClick={handleSubmit} type='submit'>Submit </button>
+  </div>;
 }
 
 function App() {
   return (
     <>
-      <h1>Task Manager!!</h1>
+      {createTitle()}
+      {createInputTitle()}
       {serveTodo()}
-
     </>
   )
 }
 
 export default App
+
+
